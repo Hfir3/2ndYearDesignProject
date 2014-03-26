41b70e8f6b43dd073dc1c7bafdf2fc5b7c898a1f
hfire <luo.hilary@gmail.com>
2014-03-26 14:14:38 -0400

    1st test code with line trackers

--- LetterAquisitionTestCode/LetterAquisitionTestCode.ino
+++ LetterAquisitionTestCode/LetterAquisitionTestCode.ino
@@ -1,12 +1,60 @@
+#include <Servo.h>
+
 //Letter Aquisition Test Code
 
+const int lineTrackerPin1 = A0;
+const int lineTrackerPin2 = A1;
+const int aquisitionMotorPin = 7;
+Servo aquisitionMotor;
+
+int lineTrackerData1 = 0;
+int lineTrackerData2 = 0;
+int deliveryAddress = -1;
+int letterInTransitFlag =0;
+
 void setup() {
+  pinMode(lineTrackerPin1, INPUT);
+  pinMode(lineTrackerPin2, INPUT);
   
+  aquisitionMotor.attach(aquisitionMotorPin);
+  
+  Serial.begin(9600);
 }
 
 
 void loop() {
+  if ((deliveryAddress == -1) && (letterInTransitFlag ==0)
+    aquisitionMotor.writeMicroseconds(1750);
+  readLineTrackers();
+  if ((lineTrackerData1 > 750)||(lineTrackerData1 < 200)) {
+  //grey is around 626, white is around 33
+    aquisitionMotor.writeMicroseconds(1500);
+    letterInTransitFlag = 1;
+    if (lineTrackerData1 < 200) {
+      if (lineTrackerData2 < 200)
+        deliveryAddress = 0;
+      else 
+        deliveryAddress = 1;
+    }
+    else if (lineTrackerData2 > 750) {
+      if (lineTrackerData2 > 750)
+        deliveryAddress = 3;
+      else 
+        deliveryAddress = 4;
+    }
+  }
+  else
+      deliveryAddress = -1;
   
-  
-  
+  Serial.println(deliveryAddress);
+  if (letterInTransitFlag ==2) {
+    aquisitionMotor.writeMicroseconds(1250);
+    delay(2000);
+    letterInTransitFlag = 0;
+  }
+}
+
+void readLineTrackers() {
+  lineTrackerData1 = analogRead(lineTrackerPin1);
+  lineTrackerData2 = analogRead(lineTrackerPin2); 
 }
